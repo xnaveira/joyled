718da866c003e8cc901ef19ec8fb3de5a3e94ae4
Xavier Naveira <xavier.naveira@ooyala.com>
2018-02-16 09:19:54 +0100

    joyled

---
+++ joyled.ino
@@ -0,0 +1,105 @@
+//http://www.instructables.com/id/Connect-and-Use-Joystick-With-Arduino/
+
+
+
+
+int UD = 0;
+
+int LR = 0;
+
+/* Arduino Micro output pins*/
+
+int DWN = 13;
+
+int UP = 12;
+
+int LEFT = 11;
+
+int RT = 10;
+
+/*Arduino Micro Input Pins */
+
+int IUP=A0;
+
+int ILR=A1;
+
+int MID = 10; // 10 mid point delta arduino, use 4 for attiny
+
+int LRMID = 0;
+
+int UPMID = 0;
+
+void setup(){
+
+pinMode(DWN, OUTPUT);
+
+pinMode(UP, OUTPUT);
+
+pinMode(LEFT, OUTPUT);
+
+pinMode(RT, OUTPUT);
+
+digitalWrite(DWN, HIGH);
+
+digitalWrite(UP, HIGH);
+
+digitalWrite(LEFT, HIGH);
+
+digitalWrite(RT, HIGH);
+
+//calabrate center
+
+LRMID = analogRead(ILR);
+
+UPMID = analogRead(IUP); }
+
+void loop(){
+
+UD = analogRead(IUP);
+
+LR = analogRead(ILR);
+
+// UP-DOWN
+
+if(UD < UPMID - MID){
+
+digitalWrite(DWN, HIGH);
+
+}else{
+
+digitalWrite(DWN, LOW);
+
+}
+
+if(UD > UPMID + MID)
+
+{ digitalWrite(UP, HIGH);
+
+}else{
+
+digitalWrite(UP, LOW);
+
+} // LEFT-RIGHT
+
+if(LR < LRMID - MID){
+
+digitalWrite(LEFT, HIGH);
+
+}else{
+
+digitalWrite(LEFT, LOW);
+
+}
+
+if(LR > LRMID + MID){
+
+digitalWrite(RT, HIGH);
+
+}else{
+
+digitalWrite(RT, LOW);
+
+}
+
+delay(400);
+}
